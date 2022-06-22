6e5a539bd038d2d293eb1e5c1ad22c01d88528c5
Shinnosuke Sawada <6warashi9@gmail.com>
2022-06-22 09:57:08 +0900

    feat: implement

---
+++ posmap.go
@@ -0,0 +1,52 @@
+package buildposmap
+
+import (
+	"go/ast"
+	"go/token"
+	"reflect"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name:             "buildposmap",
+	Doc:              "build map[token.Pos][]ast.Node",
+	Run:              run,
+	RunDespiteErrors: true,
+	Requires:         []*analysis.Analyzer{inspect.Analyzer},
+	ResultType:       reflect.TypeOf(map[token.Pos][]ast.Node(nil)),
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	return New(pass), nil
+}
+
+func New(pass *analysis.Pass) map[token.Pos][]ast.Node {
+	posMap := make(map[token.Pos][]ast.Node)
+	pass.ResultOf[inspect.Analyzer].(*inspector.Inspector).Preorder(nil, func(node ast.Node) {
+		for i := node.Pos(); i <= node.End(); i++ {
+			posMap[i] = append(posMap[i], node)
+		}
+	})
+	return posMap
+}
+
+func Node[T ast.Node](posMap map[token.Pos][]ast.Node, pos token.Pos) (node T, ok bool) {
+	for i := pos; i > 0; i-- {
+		stack := posMap[i]
+		if len(stack) == 0 {
+			break
+		}
+		for j := range stack {
+			node := stack[len(stack)-1-j]
+			ident, ok := node.(T)
+			if ok {
+				return ident, true
+			}
+		}
+	}
+	var zero T
+	return zero, false
+}
